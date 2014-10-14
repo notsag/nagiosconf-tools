e538a5b6fd6ad06f2088dbbc908a7d8dc635120e
mgaston <maxime.gaston@dauphine.fr>
2014-10-14 14:07:25 +0200

    Création des fichiers de conf par le script

--- json2nagios.py
+++ json2nagios.py
@@ -3,14 +3,16 @@
 import getopt, sys, urllib2, json
 
 data = ""
+BASECONF_HOST="define host {\nuse\tgeneric-host,ssh\ncontact_groups\tadmins\nhost_name\tHOSTNAME\naddress\tIPADDRESS\n}\n"
+BASECONF_SERVICE="define service {\nuse\tgeneric-service\nhost_name\tHOSTNAME\nservice_description\tURL\ncheck_command\tCOMMAND\n}\n"
 
+# Verification de la reponse HTTP/HTTPs
 def check_http(host, ssl=False):
     if ssl:
         url = 'https://' + host
     else: url = 'http://' + host
     try:
         connection = urllib2.urlopen(url)
-        print url + " : " + str(connection.getcode())
         connection.close()
     except urllib2.HTTPError:
         return False
@@ -50,14 +52,25 @@ json_data = open(data, 'r')
 data = json.load(json_data)
 json_data.close()
 
-# Pour chaque IP on verifie les ports 80 et 443
+# Pour chaque IP
 for row in data['data']:
-    print row[u'ip']
+    # Le hostname dans nagios correspond au premier nom
+    hn = row[u'hosts'][0]
+    config_f = open(hn+'.cfg', 'w')
+    config_host = BASECONF_HOST.replace('IPADDRESS', row[u'ip']).replace('HOSTNAME', hn)
+    # On verifie que le nom repond en http et https
     for url in row[u'hosts']:
-        check_http(url)
-        check_http(url, ssl=True)
-
-
-#TODO CREATE NAGIOS CONF
+        config_services = ""
+        # Si reponse HTTP on cree le service http
+        if check_http(url):
+            config_services = config_services + BASECONF_SERVICE.replace('HOSTNAME', hn).replace('URL', url).replace('COMMAND', 'check_myhttp!'+url)
+        # Si reponse HTTPs on cree les services https et https_certificate
+        if check_http(url, ssl=True):
+            config_services = config_services + BASECONF_SERVICE.replace('HOSTNAME', hn).replace('URL', url).replace('COMMAND', 'check_myhttps!'+url)
+            config_services = config_services + BASECONF_SERVICE.replace('HOSTNAME', hn).replace('URL', url).replace('COMMAND', 'check_myhttps_certificate!'+url)
+    # On concatene conf host et services puis on ecrit le fichier
+    conf = config_host + config_services
+    config_f.write(conf)
+    config_f.close()
 
 sys.exit(0)
