eac0bdf20f86df399535a333b4a40ed1d629d81d
mgaston <maxime.gaston@dauphine.fr>
2014-10-14 15:54:26 +0200

    Ajout d'une timeout et d'une reprise sur erreur Timeout de 10s par urls Ajout du test d'existence des fichiers de conf pour reprendre au moment de l'erreur et d'une option -f pour annuler ce mode

--- json2nagios.py
+++ json2nagios.py
@@ -5,6 +5,7 @@ import getopt, os, sys, urllib2, json
 data = ""
 output_dir = ""
 verbose = False
+force = False
 BASECONF_HOST="define host {\nuse\tgeneric-host,ssh\ncontact_groups\tadmins\nhost_name\tHOSTNAME\naddress\tIPADDRESS\n}\n"
 BASECONF_SERVICE="define service {\nuse\tgeneric-service\nhost_name\tHOSTNAME\nservice_description\tURL\ncheck_command\tCOMMAND\n}\n"
 
@@ -14,25 +15,24 @@ def check_http(host, ssl=False):
         url = 'https://' + host
     else: url = 'http://' + host
     try:
-        connection = urllib2.urlopen(url)
+        connection = urllib2.urlopen(url, timeout=10)
         connection.close()
-    except urllib2.HTTPError:
-        return False
-    except urllib2.URLError:
+    except:
         return False
     return True
 
 # Aide
 def usage():
-    print "Usage: "+sys.argv[0]+" -i <fichier> [-o <repertoire>] [-v]"
+    print "Usage: "+sys.argv[0]+" -i <fichier> [-o <repertoire>] [-v] [-f]"
     print "  -h, --help\t\taffiche ce message"
     print "  -i, --input=\t\tfichier json genere par hostnames2ip.py"
     print "  -o, --output=\t\trepertoire destination"
     print "  -v, --verbose\t\taffiche la configuration generee"
+    print "  -f, --force\t\tforce la reecriture des fichiers"
 
 # Verification des parametres
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "hi:o:v", ["help", "input=",  "output=", "verbose"])
+    opts, args = getopt.getopt(sys.argv[1:], "hi:o:vf", ["help", "input=",  "output=", "verbose", "force"])
 except getopt.GetoptError as err:
     print str(err)
     usage()
@@ -48,6 +48,8 @@ for opt,arg in opts:
         output_dir = arg
     elif opt in ("-v", "--verbose"):
         verbose = True
+    elif opt in ("-f", "--force"):
+        force = True
     else:
         sys.exit(2)
 
@@ -70,24 +72,28 @@ for row in data['data']:
     # Le hostname dans nagios correspond au premier nom
     hn = row[u'hosts'][0]
     if not output_dir == "":
-        config_f = open(output_dir+'/'+hn+'.cfg', 'w')
-    else: config_f = open(hn+'.cfg', 'w')
-    config_host = BASECONF_HOST.replace('IPADDRESS', row[u'ip']).replace('HOSTNAME', hn)
-    # On verifie que le nom repond en http et https
-    config_services = ""
-    for url in row[u'hosts']:
-        # Si reponse HTTP on cree le service http
-        if check_http(url):
-            config_services = config_services + BASECONF_SERVICE.replace('HOSTNAME', hn).replace('URL', url).replace('COMMAND', 'check_myhttp!'+url)
-        # Si reponse HTTPs on cree les services https et https_certificate
-        if check_http(url, ssl=True):
-            config_services = config_services + BASECONF_SERVICE.replace('HOSTNAME', hn).replace('URL', url).replace('COMMAND', 'check_myhttps!'+url)
-            config_services = config_services + BASECONF_SERVICE.replace('HOSTNAME', hn).replace('URL', url).replace('COMMAND', 'check_myhttps_certificate!'+url)
-    # On concatene conf host et services puis on ecrit le fichier
-    conf = config_host + config_services
-    if verbose:
-        print conf + "\n"
-    config_f.write(conf)
-    config_f.close()
+        config = output_dir+'/'+hn+'.cfg'
+    else: config = hn+'.cfg'
+    if os.path.isfile(config) and not force:
+        next
+    else: 
+        config_f = open(config,'w')
+        config_host = BASECONF_HOST.replace('IPADDRESS', row[u'ip']).replace('HOSTNAME', hn)
+        # On verifie que le nom repond en http et https
+        config_services = ""
+        for url in row[u'hosts']:
+            # Si reponse HTTP on cree le service http
+            if check_http(url):
+                config_services = config_services + BASECONF_SERVICE.replace('HOSTNAME', hn).replace('URL', url).replace('COMMAND', 'check_myhttp!'+url)
+            # Si reponse HTTPs on cree les services https et https_certificate
+            if check_http(url, ssl=True):
+                config_services = config_services + BASECONF_SERVICE.replace('HOSTNAME', hn).replace('URL', url).replace('COMMAND', 'check_myhttps!'+url)
+                config_services = config_services + BASECONF_SERVICE.replace('HOSTNAME', hn).replace('URL', url).replace('COMMAND', 'check_myhttps_certificate!'+url)
+        # On concatene conf host et services puis on ecrit le fichier
+        conf = config_host + config_services
+        if verbose:
+            print conf + "\n"
+        config_f.write(conf)
+        config_f.close()
 
 sys.exit(0)
